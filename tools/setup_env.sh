d02c3064243b30dd081cdc89212fd78bbc67bd21
jooshwells <joshwells637@gmail.com>
2025-09-02 15:19:51 -0400

    Update setup script

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -11,15 +11,6 @@ configure_env() {
 
 if [ -e "../.env" ]; then
     echo ".env already exists!"
-    read -p "Would you like to change your password? (y/n): " response
-    
-    while [[ "${response,,}" != y && "${response,,}" != n ]]; do
-        read -p "Please respond with either 'y' or 'n': " response
-    done
-
-    if [ "${response,,}" == y ]; then
-        configure_env
-    fi
 else
     configure_env
 fi
