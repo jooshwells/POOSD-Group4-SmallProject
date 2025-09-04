e45aefd380d11d8f880466c2500dd87aea603db8
Joshua Wells <144137022+jooshwells@users.noreply.github.com>
2025-09-03 22:43:55 -0400

    Quick fix to allow API to connect to database on remote server.

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -7,7 +7,7 @@
     $login = $inData["login"];
     $password = $inData["password"];
 
-    $conn = new mysqli("db", "TheBeast", "WeLoveCOP4331", "COP4331");
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
     if($conn->connect_error)
     {
         returnWithError( $conn->connect_error );
